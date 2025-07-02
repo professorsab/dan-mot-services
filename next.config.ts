a30c8dff7c5cebc83772f92a7f4f4d2abdc96d76
Muhammad Fawaz <135441198+professorsab@users.noreply.github.com>
2025-07-02 17:29:21 +0500

    test

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  "extends": ["next", "next/core-web-vitals"],
-  "rules": {
-    "react/no-unescaped-entities": "off"
-  }
+  
 };
 
 export default nextConfig;
